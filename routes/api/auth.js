1f856bada7eec03a9ede7e4e6689c57e0e47d8ea
Khaled Hamada <khaledhamada779@gmail.com>
2025-07-05 20:17:43 +0300

    Fix issues

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -58,7 +58,7 @@ router.post(
       };
       jwt.sign(
         payload,
-        process.env.jwtSecret,
+        process.env.JWT_SECRET,
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
